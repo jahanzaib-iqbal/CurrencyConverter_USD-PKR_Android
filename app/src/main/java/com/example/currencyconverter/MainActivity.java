7c77d25fc09000ba1ab376aa84c62fb9c3edf809
Jahanzaib-Iqbal <jahanzabiqbal1059@gmail.com>
2021-08-21 15:25:39 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.currencyconverter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convertNow(View view){
+        EditText dollarAmount = findViewById(R.id.dollarPr);
+        String dollarString = dollarAmount.getText().toString();
+        double dollarDouble = Double.parseDouble(dollarString);
+        double pkrDouble = dollarDouble*165;
+
+        String toastText = "price in PKR = " + Double.toString(pkrDouble) + " RS";
+
+//        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+        TextView pkrFinal = findViewById(R.id.pkrText);
+        pkrFinal.setText(toastText);
+
+    }
+//    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+}
